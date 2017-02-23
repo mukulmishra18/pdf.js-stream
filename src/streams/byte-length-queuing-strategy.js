604e7a5af40528a5f175de6f87d79619696b0d57
Mukul Mishra <mkmishra1895@gmail.com>
2017-02-24 01:02:16 +0530

    Adds umd headers.

--- src/streams/byte-length-queuing-strategy.js
+++ src/streams/byte-length-queuing-strategy.js
@@ -1,7 +1,19 @@
 'use strict';
-const { createDataProperty } = require('./helpers.js');
 
-module.exports = class ByteLengthQueuingStrategy {
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define('pdfjs/streams/byte-length-queuing-strategy',
+      ['exports', 'pdfjs/streams/helpers'], factory);
+  } else if (typeof exports !== 'undefined') {
+      factory(exports, require('./helpers.js'));
+  } else {
+      factory((root.pdfjsStreamsByteLengthQueuingStrategy = {}),
+        root.pdfjsStreamsHelpers);
+  }
+}(this, function(exports, streamsHelpers) {
+const createDataProperty = streamsHelpers.createDataProperty;
+
+class ByteLengthQueuingStrategy {
   constructor({ highWaterMark }) {
     createDataProperty(this, 'highWaterMark', highWaterMark);
   }
@@ -9,4 +21,7 @@ module.exports = class ByteLengthQueuingStrategy {
   size(chunk) {
     return chunk.byteLength;
   }
-};
+}
+
+exports.ByteLengthQueuingStrategy = ByteLengthQueuingStrategy;
+}));
--- src/streams/count-queuing-strategy.js
+++ src/streams/count-queuing-strategy.js
@@ -1,7 +1,19 @@
 'use strict';
-const { createDataProperty } = require('./helpers.js');
 
-module.exports = class CountQueuingStrategy {
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define('pdfjs/streams/count-queuing-strategy', ['exports',
+      'pdfjs/streams/helpers'], factory);
+  } else if (exports !== 'undefined') {
+    factory(exports, require('./helpers.js'));
+  } else {
+    factory((root.pdfjsStreamsCountQueuingStrategy = {}),
+      root.pdfjsStreamsHelpers);
+  }
+}(this, function(exports, streamsHelpers) {
+const createDataProperty = streamsHelpers.createDataProperty;
+
+class CountQueuingStrategy {
   constructor({ highWaterMark }) {
     createDataProperty(this, 'highWaterMark', highWaterMark);
   }
@@ -9,4 +21,7 @@ module.exports = class CountQueuingStrategy {
   size() {
     return 1;
   }
-};
+}
+
+exports.CountQueuingStrategy = CountQueuingStrategy;
+}));
